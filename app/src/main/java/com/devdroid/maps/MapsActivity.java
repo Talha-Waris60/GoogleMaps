b28957d6f27627a0d5b5654dd7d1ded36075dc11
Talha Waris <victormalik60@gmail.com>
2023-01-02 08:21:14 +0500

    Added Overlay on Map

--- app/src/main/java/com/devdroid/maps/MapsActivity.java
+++ app/src/main/java/com/devdroid/maps/MapsActivity.java
@@ -3,15 +3,18 @@ package com.devdroid.maps;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.devdroid.maps.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
@@ -34,6 +37,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(markerOptions);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(fsdLatLng));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(fsdLatLng,16f));
+
+
+        // Map Overlay as Circle
+        mMap.addCircle(new CircleOptions()
+                .center(fsdLatLng)
+                .radius(1000)
+                .fillColor(Color.GREEN)
+                .strokeColor(Color.BLACK));
+
+        // Polygon
+        mMap.addPolygon(new PolygonOptions().add(new LatLng(31.450365,73.134964),
+                new LatLng(31.450365,74.134964),
+                new LatLng(32.450365,75.134964),
+                new LatLng(32.450365,76.134964),
+                new LatLng(31.450365,77.134964))
+                .fillColor(Color.GRAY)
+                .strokeColor(Color.RED));
 
     }
 }
